639aec72d79938ab24a377ab3ac679ed7135369e
sabriBidal <sabribidal@gmail.com>
2023-06-21 22:42:37 -0300

    entrega 2, JS

--- js.js
+++ js.js
@@ -1,62 +1,113 @@
-// Precios de los productos
-let precios = {
-    producto1: 10,
-    producto2: 20,
-    producto3: 30,
-};
-  
-// Selección de productos (usuario)
-function seleccionarProductos(){
-    const productosSeleccionados = [];
-    let continuar = true;
-  
-    while (continuar){
-      const seleccion = prompt(`Seleccione un producto:
-        1. Harina
-        2. Galletitas
-        3. Fruta
-        (Ingrese "fin" para terminar y ver el precio total.)`);
+// Objeto con los productos y sus precios
+const productos = [
+    { nombre: 'Harina', precio: 10 },
+    { nombre: 'Galletitas', precio: 20 },
+    { nombre: 'Fruta', precio: 30 },
+];
 
-    switch (seleccion){
-        case '1':
-            productosSeleccionados.push('producto1');
-            console.log("Harina $10")
-            break;
-        case '2':
-            productosSeleccionados.push('producto2');
-            console.log("Galletitas $20")
-            break;
-        case '3':
-            productosSeleccionados.push('producto3');
-            console.log("Fruta $30")
-            break;
-        case 'fin':
-            continuar = false;
-            break;
-        default:
-            alert('Selección inválida. Intente nuevamente.');
+// Función para mostrar el menú de productos
+function mostrarMenu() {
+    let menu = 'Seleccione un producto:\n';
+
+    for (let i = 0; i < productos.length; i++) {
+        menu += `${i + 1}. ${productos[i].nombre} - $${productos[i].precio}\n`;
+    }
+
+    menu += '(Ingrese "fin" para terminar y ver el precio total.)';
+
+    return menu;
+}
+
+// Función para seleccionar productos
+function seleccionarProductos() {
+    const productosSeleccionados = [];
+
+    while (true) {
+        const seleccion = prompt(mostrarMenu());
+
+        if (seleccion === 'fin') {
             break;
         }
+
+        const indiceProducto = parseInt(seleccion) - 1;
+
+        if (indiceProducto >= 0 && indiceProducto < productos.length) {
+            const producto = productos[indiceProducto];
+            productosSeleccionados.push(producto);
+            alert(`Producto agregado: ${producto.nombre}`);
+        } else {
+            alert('Selección inválida. Intente nuevamente.');
+        }
     }
-  
+
     return productosSeleccionados;
 }
-  
-// Calcular el costo total de los productos seleccionados
-function calcularCostoTotal(productosSeleccionados){
+
+// Función para calcular el costo total de los productos seleccionados
+function calcularCostoTotal(productosSeleccionados) {
     let costoTotal = 0;
 
-    for (let i = 0; i <= productosSeleccionados.length; i++){
-    const producto = productosSeleccionados[i];
-    if (precios.hasOwnProperty(producto)){
-        costoTotal += precios[producto];
-        }
+    for (let i = 0; i < productosSeleccionados.length; i++) {
+        const producto = productosSeleccionados[i];
+        costoTotal += producto.precio;
     }
-  
+
     return costoTotal;
 }
-  
 
-const productosSeleccionados = seleccionarProductos();
-const costoTotal = calcularCostoTotal(productosSeleccionados);
-alert(`El costo total de los productos seleccionados es: $${costoTotal}`);
+// Función para mostrar los productos seleccionados
+function mostrarProductosSeleccionados(productosSeleccionados) {
+    let listaProductos = 'Productos seleccionados:\n';
+
+    for (let i = 0; i < productosSeleccionados.length; i++) {
+        const producto = productosSeleccionados[i];
+        listaProductos += `${producto.nombre} - $${producto.precio}\n`;
+    }
+
+    alert(listaProductos);
+}
+
+// Función para quitar un producto de la lista de seleccionados
+function quitarProducto(productosSeleccionados) {
+    const seleccion = prompt('Ingrese el número del producto a quitar:');
+    const indiceProducto = parseInt(seleccion) - 1;
+
+    if (indiceProducto >= 0 && indiceProducto < productosSeleccionados.length) {
+        const productoQuitado = productosSeleccionados.splice(indiceProducto, 1)[0];
+        alert(`Producto quitado: ${productoQuitado.nombre}`);
+    } else {
+        alert('Selección inválida. Intente nuevamente.');
+    }
+}
+
+// Menú principal
+function menuPrincipal() {
+    const productosSeleccionados = seleccionarProductos();
+    mostrarProductosSeleccionados(productosSeleccionados);
+    const costoTotal = calcularCostoTotal(productosSeleccionados);
+    alert(`El costo total de los productos seleccionados es: $${costoTotal}`);
+
+    while (true) {
+        const opcion = prompt('¿Desea agregar otro producto (1), quitar un producto (2) o finalizar (3)?');
+
+        switch (opcion) {
+            case '1':
+                const nuevosProductos = seleccionarProductos();
+                productosSeleccionados.push(...nuevosProductos);
+                mostrarProductosSeleccionados(nuevosProductos);
+                break;
+            case '2':
+                quitarProducto(productosSeleccionados);
+                break;
+            case '3':
+                alert('Gracias por su compra. ¡Hasta luego!');
+                return;
+            default:
+                alert('Opción inválida. Intente nuevamente.');
+                break;
+        }
+    }
+}
+
+// Ejecutar el menú principal
+menuPrincipal();
